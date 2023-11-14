fb50a900f14a45a9119bc8c3e0ceff3f290c25b7
Nikita Dyakonov <roboromapie@gmail.com>
2023-11-14 03:34:43 +0300

    first commit

---
+++ target/debug/deps/sum_and_mean-10c1c003fe002b23.d
@@ -0,0 +1,5 @@
+C:\Users\robor\sum_and_mean\target\debug\deps\sum_and_mean-10c1c003fe002b23.rmeta: src\main.rs
+
+C:\Users\robor\sum_and_mean\target\debug\deps\sum_and_mean-10c1c003fe002b23.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/sum_and_mean-dd06903c4092657c.d
@@ -0,0 +1,5 @@
+C:\Users\robor\sum_and_mean\target\debug\deps\sum_and_mean-dd06903c4092657c.rmeta: src\main.rs
+
+C:\Users\robor\sum_and_mean\target\debug\deps\sum_and_mean-dd06903c4092657c.d: src\main.rs
+
+src\main.rs:
