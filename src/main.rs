b300c2da4c6f121dc1aa7635e52c0cb1396387f1
Nikita Dyakonov <roboromapie@gmail.com>
2023-11-14 03:36:13 +0300

    second commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Не удалось прочитать строку");
+
+    let numbers: Vec<i32> = input
+        .trim()
+        .split(", ")
+        .map(|s| i32::from_str(s).expect("Не удалось преобразовать строку в число"))
+        .collect();
+    
+    let mut sum = 0;
+
+    for &number in &numbers {
+        sum += number;
+    }
+
+    let mean = sum as f32 / numbers.len() as f32;
+
+    println!("Сумма равна {sum}, арифметическое среднее равно {mean}")
 }
